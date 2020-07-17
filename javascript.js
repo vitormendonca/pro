0cb5890586a9d51d5416316c3dc3471930825bd7
Vitor Ferreira Silva de Mendonça <vitor.fsm@hotmail.com>
2020-07-17 11:15:37 -0300

    Revert "Update javascript.js"
    This reverts commit b2598602d9d5ff41a7526276c432c5c889dcec85.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
+
 const header = document.querySelector('header');
 const headernav = document.querySelector('header nav');
 
-
-/*Create MenuIcon*/
 const adicionarMenuIcon = () => {
+
     const divspan = document.createElement('div');
     const span = document.createElement('span');
     const imgicon = document.createElement('img');
@@ -16,48 +16,59 @@ const adicionarMenuIcon = () => {
     span.appendChild(imgicon);
 
     divspan.style.width = '100%';
-    divspan.style.display = 'flex';
-    divspan.style.justifyContent = 'space-between';
+    divspan.style.display ='flex';
+    divspan.style.justifyContent= 'space-between';
     divspan.style.alignItems = 'center';
 
     span.style.cursor = 'pointer';
     span.style.height = '40px'
-    imgicon.setAttribute('src', 'imgs/logos/menuicon.png');
+    imgicon.setAttribute('src' , 'imgs/logos/menuicon.png');
     imgicon.setAttribute('alt', 'menuicon');
     imgicon.style.width = '100%';
 }
-/*Remove Menu Icon*/
-const removerMenuIcon = () => {
+
+const removerMenuIcon = () =>{
     const logodiv = document.querySelector('#logopro');
     const divspan = header.querySelector('div');
-    header.replaceChild(logodiv, divspan);
+    header.replaceChild(logodiv,divspan);
 }
-/*Toggle Navigation on Mobile Devices*/
-const toggleNav = () => {
+
+const toggleNav = () =>{
     let clicked = false;
     const imgicon = document.querySelector('span img');
-    imgicon.addEventListener('click', function (event) {
-        if (!clicked) {
+    imgicon.addEventListener('click', function(event){
+        if(!clicked){
             headernav.style.display = "block";
-            clicked = true;
-        } else {
+           clicked = true;
+        }
+        else{
             headernav.style.display = "none";
             clicked = false;
         }
     })
 }
 
-/*Functions for window less than 650px (onresize), toggle icon, toggle nav*/
-window.addEventListener("resize", function () {
+
+window.addEventListener("resize", function() {
     if (window.innerWidth < 650) {
-        adicionarMenuIcon();
+       adicionarMenuIcon();
         toggleNav();
-    } else {
+    }
+    else{
         removerMenuIcon();
     }
-})
-/*Functions for window less than 650px, toggle icon, toggle nav*/
-if (window.innerWidth < 650) {
+ })
+
+ if(window.innerWidth < 650){
     adicionarMenuIcon();
     toggleNav();
+ }
+
+
+
+function slide () {
+    const slideimg = document.getElementById('slideitem');
+    let currentimg = 'imgs/treinojudo/treino16.png';
 }
+
+slide()
